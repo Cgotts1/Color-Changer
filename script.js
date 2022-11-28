c457d464875d784a3de95f5d2eca64519c4143a8
Cgotts <cobi.gottschalk@gmail.com>
2022-11-27 16:25:07 -0800

    Cleaned up code

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-let box = document.querySelector("#square-container")
-// let body = document.querySelector("body")
+let box = document.querySelector(".square-container")
 
-
-
-// let randomColor = ("rgb(" + Math.floor(Math.random() * 255)
-// + "," + Math.floor(Math.random() * 255) + ","
-// + Math.floor(Math.random() * 255) + ")");
-
-
-
-// let clickBox = ()=>{
-//     box.style.backgroundColor = randomColor;
-// }
-
-// box.addEventListener("click", clickBox)
 
 function clickBox(){
+
     let randomColor = ("rgb(" + Math.floor(Math.random() * 255)
 + "," + Math.floor(Math.random() * 255) + ","
 + Math.floor(Math.random() * 255) + ")");
+
     box.style.backgroundColor = randomColor;
-    // body.style.backgroundColor = randomColor;
 }
 
 box.addEventListener("click", clickBox)
